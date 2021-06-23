18a1101ba2a373d49de7b79311ff179980000582
Lukas Kohlmaier <luko0610@gmail.com>
2021-06-23 21:28:06 +0200

    - Implemented request fragment and necessary data classes.

---
+++ app/src/main/java/com/interactive/buddy/data/RequestStore.kt
@@ -0,0 +1,21 @@
+package com.interactive.buddy.data
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.interactive.buddy.data.model.Request.Request
+import com.interactive.buddy.data.model.Request.RequestType
+import java.time.LocalDateTime
+import java.util.*
+
+class RequestStore {
+    @RequiresApi(Build.VERSION_CODES.O)
+    companion object {
+        fun getRequests() : List<Request> = listOf<Request>(
+            Request(UUID.randomUUID(), "Thomas", 5, RequestType.HAPPU, LocalDateTime.now()),
+            Request(UUID.randomUUID(), "Heinz", 1, RequestType.DEPRESSED, LocalDateTime.now()),
+            Request(UUID.randomUUID(), "Lisa", 10, RequestType.JUSTTALK, LocalDateTime.now()),
+            Request(UUID.randomUUID(), "Katharina", 3, RequestType.BORED, LocalDateTime.now()),
+            Request(UUID.randomUUID(), "Christina", 2, RequestType.HAPPU, LocalDateTime.now()),
+        )
+    }
+}

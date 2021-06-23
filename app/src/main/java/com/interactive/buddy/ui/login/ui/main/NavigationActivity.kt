0bd4db0b29e50701ab31eaf7be4d00f2d03b17b2
Lukas Kohlmaier <luko0610@gmail.com>
2021-06-23 22:11:29 +0200

    - Added Navigation activity and wired request fragment together.

---
+++ app/src/main/java/com/interactive/buddy/ui/login/ui/main/NavigationActivity.kt
@@ -0,0 +1,34 @@
+package com.interactive.buddy.ui.login.ui.main
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.interactive.buddy.R
+import com.interactive.buddy.databinding.ActivityNavigationBinding
+
+class NavigationActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityNavigationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+     binding = ActivityNavigationBinding.inflate(layoutInflater)
+     setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_navigation)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_chats, R.id.navigation_requests, R.id.navigation_mood_selector, R.id.navigation_karma, R.id.navigation_profile))
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}

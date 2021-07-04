a591c79a68112db5fbcefddf96a2919cc43521d6
Lukas Kohlmaier <luko0610@gmail.com>
2021-07-04 23:19:44 +0200

    ADD - Implemented fetching own requests.

--- app/src/main/java/com/interactive/buddy/data/URLs.kt
+++ app/src/main/java/com/interactive/buddy/data/URLs.kt
@@ -5,4 +5,5 @@ object URLs {
     const val URL_REGISTER = ROOT_URL + "register"
     const val URL_LOGIN = ROOT_URL + "login"
     const val URL_REQUESTS = ROOT_URL + "requests"
+    const val URL_OWN_REQUESTS = "$URL_REQUESTS/own"
 }
